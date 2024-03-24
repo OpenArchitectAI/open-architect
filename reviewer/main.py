07bf626d629d37e45a1fc794b9fd64784bcdccc5
Aiswarya Sankar <aiswarya@Aiswaryas-MacBook-Pro.local>
2024-03-23 23:10:01 -0700

    Fix small issues and set up architect

---
+++ reviewer/main.py
@@ -0,0 +1,9 @@
+import pygithub
+
+def review_code(pr, codebase):
+    # This function is responsible for reviewing the code
+    # It should take each of the PR comments, the associated code chunks and then return updated code that actually fixes the issues 
+    # It should update the PR that is passed in and return that diff 
+    updatedDiff = ""
+    return updatedDiff 
+

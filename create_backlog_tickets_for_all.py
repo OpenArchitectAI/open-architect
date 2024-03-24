f18cefdca14e02f4929f03f167bb8a0ce708529e
Pierre Collignon <picolli@amazon.com>
2024-03-24 15:34:58 -0700

    Script to generate backlog items

---
+++ create_backlog_tickets_for_all.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from random import randint
+import os
+
+from models import Ticket
+from trello_helper import TrelloHelper
+
+load_dotenv()
+
+N_TICKETS = 10
+
+trello_api_key = os.getenv("TRELLO_API_KEY")
+trello_api_secret = os.getenv("TRELLO_API_SECRET")
+trello_token = os.getenv("TRELLO_TOKEN")
+trello_board_id = os.getenv("TRELLO_BOARD_ID")
+
+th = TrelloHelper(trello_api_key, trello_token, trello_board_id)
+
+tickets = [Ticket(
+    title="Test Ticket " + str(randint(0, 10000)),
+    description="This is a test ticket",
+) for _ in range(N_TICKETS)]
+th.push_tickets_to_backlog_and_assign(tickets)

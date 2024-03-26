3a4332dff605a51d4c7f2c3384244c0d4fb3e732
Aiswarya Sankar <aiswarya@Aiswaryas-MacBook-Pro.local>
2024-03-25 20:01:46 -0700

    rebase

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+
 from dotenv import load_dotenv
 from threading import Thread
 import os
@@ -13,12 +14,8 @@ import src.chat_interface as chat_interface
 load_dotenv()
 
 gh_repo = os.getenv("GITHUB_REPO_URL")
-<<<<<<< HEAD
 gh_api_token_intern = os.getenv("GITHUB_TOKEN_INTERN")
 gh_api_token_reviewer = os.getenv("GITHUB_TOKEN_REVIEWER")
-=======
-gh_api_token = os.getenv("GITHUB_TOKEN")
->>>>>>> 746ae7a (Integrate review with Trello)
 openai_api_key = os.getenv("OPENAI_API_KEY")
 trello_api_key = os.getenv("TRELLO_API_KEY")
 trello_api_secret = os.getenv("TRELLO_API_SECRET")
@@ -27,12 +24,8 @@ trello_board_id = os.getenv("TRELLO_BOARD_ID")
 
 if (
     gh_repo is None
-<<<<<<< HEAD
     or gh_api_token_intern is None
     or gh_api_token_reviewer is None
-=======
-    or gh_api_token is None
->>>>>>> 746ae7a (Integrate review with Trello)
     or openai_api_key is None
     or trello_api_key is None
     or trello_api_secret is None
@@ -47,33 +40,19 @@ if (
 # Write them back to the .env file
 with open(".env", "w") as f:
     f.write(f"GITHUB_REPO_URL={gh_repo}\n")
-<<<<<<< HEAD
     f.write(f"GITHUB_TOKEN_INTERN={gh_api_token_intern}\n")
     f.write(f"GITHUB_TOKEN_REVIEWER={gh_api_token_reviewer}\n")
-=======
-    f.write(f"GITHUB_TOKEN={gh_api_token}\n")
->>>>>>> 746ae7a (Integrate review with Trello)
     f.write(f"OPENAI_API_KEY={openai_api_key}\n")
     f.write(f"TRELLO_API_KEY={trello_api_key}\n")
     f.write(f"TRELLO_API_SECRET={trello_api_secret}\n")
     f.write(f"TRELLO_TOKEN={trello_token}\n")
     f.write(f"TRELLO_BOARD_ID={trello_board_id}\n")
 
-<<<<<<< HEAD
 gh_helper_intern = GHHelper(gh_api_token_intern, gh_repo)
 gh_helper_reviewer = GHHelper(gh_api_token_reviewer, gh_repo)
 trello_helper = TrelloHelper(trello_api_key, trello_token, trello_board_id)
 
 intern = Intern("alex", gh_helper=gh_helper_intern, trello_helper=trello_helper)
-=======
-gh_helper = GHHelper(gh_api_token, gh_repo)
-trello_helper = TrelloHelper(trello_api_key, trello_token, trello_board_id)
-
-# Step 1: With User input (streamit), define tickets, push to Trello's Backlog
-# architect = Architect("dave")
-intern1 = Intern("alex", gh_helper=gh_helper, trello_helper=trello_helper)
-# intern2 = Intern("bob", gh_helper=gh_helper, trello_helper=trello_helper)
->>>>>>> 746ae7a (Integrate review with Trello)
 reviewer = Reviewer(
     "charlie",
     gh_helper=gh_helper_reviewer,
@@ -87,18 +66,6 @@ reviewer_thread = Thread(target=reviewer.run)
 # chat_interface.open_architect(trello_helper, gh_helper_reviewer)
 
 while True:
-<<<<<<< HEAD
     # Step 2: Let's get to work (n + 1 threads)
     intern_thread.start()
     reviewer_thread.start()
-=======
-    # Console interface/Chat with
-    # architect.start_cutting_tickets_for_interns(TrelloHelper)
-
-    # Show Trello's Backlog: everyone is like "WOW!"
-
-    # Step 2: Let's get to work (3 threads) (show dashboard/console to make refresh queries + Trello and GH updates)
-    # intern1.run()
-    # intern2.run()
-    reviewer.run()
->>>>>>> 746ae7a (Integrate review with Trello)

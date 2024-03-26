43b33915773e5bc3d94a102ffb747969ba94b824
Aiswarya Sankar <aiswarya@Aiswaryas-MacBook-Pro.local>
2024-03-25 19:41:28 -0700

    rebase again

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
+
 class TicketStatus(Enum):
     BACKLOG = "Backlog"
+    TODO = "To Do"
     WIP = "WIP"
     READY_FOR_REVIEW = "Ready for Review"
     REVIEWED = "Reviewed"
